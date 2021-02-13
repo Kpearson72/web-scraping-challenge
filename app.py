240f5bc95938000e23f47a010a2bc5c4f4482d82
Kpearson72 <kpearson72@gmail.com>
2021-02-12 22:03:39 -0600

    finally added all my hemisphere images to index, deleted scrape_mars2.py and added scrape_2.py, changed code in notebook to add code to table and change appearance

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars2
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -19,19 +18,19 @@ def index():
     # Return template and data
     return render_template("index.html", mars=mars)
 
-
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars2.scrape_all()
+    mars_data = scrape_mars.scrape_all()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect('/')
+    return redirect('/', code=302)
+
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Mission to Mars
-
-
 # Import Dependencies
 import requests
 import pymongo
@@ -12,23 +6,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 from splinter import Browser
 import time
 
+
 # # Step 1 - Scraping
 # ### Complete your initial scraping using Jupyter Notebook, BeautifulSoup, Pandas, and Requests/Splinter.
 
 # ## NASA Mars News
 # ### Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text. Assign the text to variables that you can reference later.
 
-
 def scrape_all():
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news'
 
-    # # Retrieve page with the requests module
-    # response = requests.get(url)
 
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+
+
 
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
@@ -36,10 +30,12 @@ def scrape_all():
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Extract the title and paragraph text
+
+
+    # Extract the title and paragraph text 
     article = soup.find("div", class_='list_text')
     news_title = article.find("div", class_="content_title").text
-    news_p = article.find("div", class_="article_teaser_body").text
+    news_p = article.find("div", class_ ="article_teaser_body").text
     print(news_title)
     print(news_p)
 
@@ -49,69 +45,110 @@ def scrape_all():
     news_title = "NASA's Perseverance Pays Off Back Home"
     news_p = "Even as the Perseverance rover approaches Mars, technology on board is paying off on Earth."
 
+
+
     # Quit browser
     browser.quit()
-    
+
+
     # ## # JPL Mars Space Images - Featured Image - link doesn't work - Instructor said to skip - I used the example url given
+
+
     featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA16225_hires.jpg'
+
 
     # ## Mars Facts
 
     # ### Visit the Mars Facts webpage and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
-    #
+    # 
     # ### Use Pandas to convert the data to a HTML table string.
 
-    # Import Pandas to scrape table and clean table
+
+    #Import Pandas to scrape table and clean table
     import pandas as pd
+
+
 
     # add a variable to the url
     url = 'https://space-facts.com/mars/'
+
+
+
+    response = requests.get(url)
+
+
+
+
+    soup = bs(response.text, "lxml") 
+
+
 
     # Use read_html function in Pandas to scrape tabular data from a page
     tables = pd.read_html(url)
     tables
 
+
+
     # check what I retrieved
     type(tables)
+
+
 
     # create a dataframe from the list of tables
     df = tables[0]
     print(df)
 
+
+
     # Rename column headers
-    df_rename = df.rename(columns={0: "Description", 1: "Mars"})
+    df_rename = df.rename(columns={0:"Description", 1: "Mars"})
     df_rename
+
+
 
     # Index Description column
     df_index = df_rename.set_index('Description')
     df_index
 
-    # Use pandas to convert the data to a HTML table strin
+
+
+    # Use pandas to convert the data to a HTML table string
     html_table = df_index.to_html()
     html_table
 
+
+
     # clean up html data string, by getting rid of \n
-    html_table=html_table.replace('\n', '')
+    html_table = html_table.replace('\n', '')
     html_table
+
+
+
+    html_table = html_table.replace('right', 'left')
+    html_table = html_table.replace('dataframe', 'table table-striped')
+    html_table
+
 
     # ## Mars Hemispheres
     # #### Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres.
-    #
-    #
+    # 
+    # 
     # #### You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
-    #
-    #
+    # 
+    # 
     # #### Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys img_url and title.
-    #
-    #
+    # 
+    # 
     # #### Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
-    #
-    #
-    #
+    # 
+    # 
+    # 
+
 
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+
 
     # URL to be scraped
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -119,7 +156,6 @@ def scrape_all():
     # visit the astrogeology website
     browser.visit(url)
     time.sleep(1)
-
     html = browser.html
     soup = bs(html, "html.parser")
     hem_image = []
@@ -136,32 +172,30 @@ def scrape_all():
         image_link = "https://astrogeology.usgs.gov/" + link_end
         # visit browser page using the link
         browser.visit(image_link)
-        time.sleep(1)
         # html object
         html = browser.html
         # Create BeautifulSoup object; parse with "html.parser"
-        soup = bs(html, "html.parser")
+        soup=bs(html, "html.parser")
         # Retreive all elements that contain planets' title and url
-        download = soup.find("div", class_="downloads")
-        hemisphere_img_url = download.find("a")["href"]
-        hem_image.append({"title": title, "img_url": hemisphere_img_url})
+        download= soup.find("div", class_="downloads")
+        image_url = download.find("a")["href"]
+        hem_image.append({"title":title, "img_url": image_url})
+
 
 
 
     # show list of dictionaries
-    # print(hem_image)
-    
+    print(hem_image)
 
 
+
+
+    # hem_image  = hemisphere_image_urls
     hemisphere_image_urls = [
-        {'title': 'Cerberus Hemisphere Enhanced',
-            'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'},
-        {'title': 'Schiaparelli Hemisphere Enhanced',
-            'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'},
-        {'title': 'Syrtis Major Hemisphere Enhanced',
-            'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'},
-        {'title': 'Valles Marineris Hemisphere Enhanced',
-            'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}
+                            {'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, 
+                            {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, 
+                            {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, 
+                            {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}
 
     ]
 
@@ -176,8 +210,6 @@ def scrape_all():
     }
 
     # Quit browser
+    
     browser.quit()
     return mars_data
-
-
-
